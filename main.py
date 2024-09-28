968e1c8b7e8cdc74e75801083e124ca4b0b1985d
Chaitanya Lohani <109726533+AloneFFOfficial@users.noreply.github.com>
2024-09-28 05:40:53 +0000

    fix dotenv issue.

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 import spacy
 import difflib
 import os
-from dotenv import load_env
-load_env()
+from dotenv import load_dotenv
+load_dotenv()
 
 '''Initialize spaCy English model'''
 nlp = spacy.load("en_core_web_sm")
